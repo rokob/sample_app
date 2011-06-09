7fa420da16e5177debe870944fb9f9f9c9f56835
Andrew Ledvina <wvvwwvw@gmail.com>
2011-06-09 19:36:20 -0400

    User model with passwords, and user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @title = "Show"
+    @title = @user.name
   end
 
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+# By using the symbol ':user', we get Factory Girl
+# to simulate the User model.
+Factory.define :user do |user|
+  user.name                   "Andrew Wolfhouse"
+  user.email                  "awolf@example.com"
+  user.password               "foobar42"
+  user.password_confirmation  "foobar42"
+end
